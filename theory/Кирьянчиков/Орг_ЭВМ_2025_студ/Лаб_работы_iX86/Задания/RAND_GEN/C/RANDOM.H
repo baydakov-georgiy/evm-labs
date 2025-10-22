050c504d51a6c91a6c1194a2b20d57a4408c6b74
baydakov-georgiy <lespend33@gmail.com>
2025-10-22 16:24:51 +0300

    lb3

---
+++ theory/Кирьянчиков/Орг_ЭВМ_2025_студ/Лаб_работы_iX86/Задания/RAND_GEN/C/RANDOM.H
@@ -0,0 +1,9 @@
+/* random.h
+
+   Copyright (c) 1995 by D.N.Kirshin, LEEI Reseach Center
+   All Rights Reserved.
+
+*/
+void dnk_randomize(void);
+double dnk_random(void);
+double dnk_normal(double,double);
